1d0df48711e7a0524b155f279c5de98423ef8954
Chakrit <chakritkp@hotmail.com>
2023-12-04 00:16:16 +0700

    add style search input

--- src/page/CardsList.jsx
+++ src/page/CardsList.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   cardListSelector,
   fetchCards,
-  addCard
+  addCard,
 } from "../hook/store/slices/cardListSlice";
 import searchIcon from "../assets/search.png";
 import Card from "../component/card";
@@ -16,8 +16,7 @@ const CardsList = () => {
 
   useEffect(() => {
     dispatch(fetchCards());
-  }, [dispatch,datalist]);
-  
+  }, [dispatch, datalist]);
 
   useEffect(() => {
     if (searchTerm === "") {
@@ -41,37 +40,40 @@ const CardsList = () => {
       happiness: event.happiness,
     };
     dispatch(addCard(newCard));
-    
   };
 
   return (
-    <main className="absolute z-50 w-[800px] bg-white flex flex-col gap-3 py-3 justify-center items-center left-1/2 top-0 transform -translate-x-1/2 bg-transparent">
-      <div className="w-full px-2">
-        <input
-          type="text"
-          className="searchBoxBorder w-full h-[50px] p-3 text-black"
-          placeholder="Find pokemon"
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <img
-          src={searchIcon}
-          className="absolute top-3 right-3 w-[50px]"
-          alt={searchIcon}
-        />
+    <main className="absolute z-50 w-[800px] bg-white flex flex-col gap-3 py-3 justify-center items-center left-1/2 top-2/4 transform -translate-x-1/2 -translate-y-1/2 bg-transparent">
+      <div className="w-full h-screen overflow-x-auto">
+        <div className="w-full h-[60px] flex flex-col items-center fixed z-40 bg-white">
+          <input
+            type="text"
+            className="searchBoxBorder w-[95%] mt-2 h-[50px] p-3 text-black"
+            placeholder="Find pokemon"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <img
+            src={searchIcon}
+            className="absolute top-2 right-5 w-[50px]"
+            alt={searchIcon}
+          />
+        </div>
+        <div className="w-full flex flex-col items-center mt-16">
+          {cardList.map((cardData) => (
+            <Card
+              key={cardData.id}
+              id={cardData.id}
+              img={cardData.imageUrl}
+              name={cardData.name}
+              hp={cardData.hpValue}
+              str={cardData.strLevel}
+              weak={cardData.weakLevel}
+              happiness={cardData.happiness}
+              handleAddCard={handleAddCard}
+            />
+          ))}
+        </div>
       </div>
-      {cardList.map((cardData) => (
-        <Card
-          key={cardData.id}
-          id={cardData.id}
-          img={cardData.imageUrl}
-          name={cardData.name}
-          hp={cardData.hpValue}
-          str={cardData.strLevel}
-          weak={cardData.weakLevel}
-          happiness={cardData.happiness}
-          handleAddCard={handleAddCard}
-        />
-      ))}
     </main>
   );
 };
